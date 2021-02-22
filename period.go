e5c3fc6f0c09a0aa67609c264c51ebc1bb0326ad
Louis FRADIN <louis.fradin@gmail.com>
2021-02-22 20:20:05 +0100

    period: add struct

---
+++ period.go
@@ -0,0 +1,32 @@
+package models
+
+const (
+	// M1 represents 1 minute in epoch time
+	M1 int64 = 60
+	// M3 represents 3 minutes in epoch time
+	M3 int64 = 3 * 60
+	// M5 represents 5 minutes in epoch time
+	M5 int64 = 5 * 60
+	// M15 represents 15 minutes in epoch time
+	M15 int64 = 15 * 60
+	// M30 represents 30 minutes in epoch time
+	M30 int64 = 30 * 60
+	// H1 represents 1 hour in epoch time
+	H1 int64 = 1 * 60 * 60
+	// H2 represents 2 hours in epoch time
+	H2 int64 = 2 * 60 * 60
+	// H4 represents 4 hours in epoch time
+	H4 int64 = 4 * 60 * 60
+	// H6 represents 6 hours in epoch time
+	H6 int64 = 6 * 60 * 60
+	// H8 represents 8 hours in epoch time
+	H8 int64 = 8 * 60 * 60
+	// H12 represents 12 hours in epoch time
+	H12 int64 = 12 * 60 * 60
+	// D1 represents 1 day in epoch time
+	D1 int64 = 1 * 24 * 60 * 60
+	// D3 represents 3 days in epoch time
+	D3 int64 = 3 * 24 * 60 * 60
+	// W1 represents 1 week in epoch time
+	W1 int64 = 1 * 7 * 24 * 60 * 60
+)
